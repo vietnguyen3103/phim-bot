41dc5cbc0325105f3763e82cbfbb7d9b0ddc932e
unknown <thanhquy789@gmail.com>
2020-05-30 21:07:43 +0700

    Add handleMessage

--- index.js
+++ index.js
@@ -29,8 +29,10 @@ app.post("/webhook", (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        // handleMessage(sender_psid, webhook_event.message);
+        //Ở đay sẽ xử lý text
+        handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
+        //dưới đây xử lý ví dụ button gọi lại
         handlePostback(sender_psid, webhook_event.postback);
       }
     });
@@ -66,51 +68,51 @@ app.get("/webhook", (req, res) => {
   }
 });
 
-// function handleMessage(sender_psid, received_message) {
-//   let response;
+function handleMessage(sender_psid, received_message) {
+  let response;
 
-//   // Checks if the message contains text
-//   if (received_message.text) {
-//     // Create the payload for a basic text message, which
-//     // will be added to the body of our request to the Send API
-//     response = {
-//       text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
-//     };
-//   } else if (received_message.attachments) {
-//     // Get the URL of the message attachment
-//     let attachment_url = received_message.attachments[0].payload.url;
-//     response = {
-//       attachment: {
-//         type: "template",
-//         payload: {
-//           template_type: "generic",
-//           elements: [
-//             {
-//               title: "Is this the right picture?",
-//               subtitle: "Tap a button to answer.",
-//               image_url: attachment_url,
-//               buttons: [
-//                 {
-//                   type: "postback",
-//                   title: "Yes!",
-//                   payload: "yes",
-//                 },
-//                 {
-//                   type: "postback",
-//                   title: "No!",
-//                   payload: "no",
-//                 },
-//               ],
-//             },
-//           ],
-//         },
-//       },
-//     };
-//   }
+  // Checks if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      text: `Tin nhắn vừa nhập: "${received_message.text}".`,
+    };
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+  }
 
-//   // Send the response message
-//   callSendAPI(sender_psid, response);
-// }
+  // Send the response message
+  callSendAPI(sender_psid, response);
+}
 
 async function handlePostback(sender_psid, received_postback) {
   let response;
